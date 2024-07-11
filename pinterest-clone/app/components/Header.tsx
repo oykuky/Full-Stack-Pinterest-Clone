36a94a965d20f801fd10fed571091821c03fa4e7
Öykü Kaya <oykuky001@gmail.com>
2024-07-11 23:36:47 +0300

    router added

--- pinterest-clone/app/components/Header.tsx
+++ pinterest-clone/app/components/Header.tsx
@@ -40,11 +40,12 @@ function Header() {
   };
    
   const router = useRouter();
+
   return (
     <div className='flex justify-between gap-3 md:gap-2 items-center p-6 '>
         <Image src='/logo.png' alt="logo" width={50} height={50}
          className='hover:bg-gray-300 p-3 rounded-full curser-pointer'/>
-        <button className='text-white bg-black p-3 px-4 rounded-full hidden md:block '>
+        <button  onClick={()=>router.push('/')} className='text-white bg-black p-3 px-4 rounded-full hidden md:block '>
              Home
         </button>
         <button className='p-3 px-4 rounded-full hidden md:block font-semibold'>
@@ -60,7 +61,9 @@ function Header() {
         <HiChat className="text-[50px] text-gray-500 hover:bg-gray-200 rounded-full " />
         
         { session?.user? 
-        <Image src={session.user.image || '/default-user.png'} width={50} height={50} alt='user-img' 
+        <Image src={session.user.image || '/default-user.png'} 
+         onClick={()=>router.push('/'+ session?.user?.email)}
+         width={50} height={50} alt='user-img' 
          className='hover:bg-gray-300 p-2 rounded-full curser-pointer'/>  
           :
         <button className='p-3 px-4 rounded-full hidden md:block font-semibold' onClick={ ()=> signIn() }>
