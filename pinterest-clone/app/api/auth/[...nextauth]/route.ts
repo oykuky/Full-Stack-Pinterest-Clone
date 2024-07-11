e3f330aff7b42b501f8e1ab2e9c7ba2fc52aee73
Öykü Kaya <oykuky001@gmail.com>
2024-07-11 12:18:34 +0300

    api key added

---
+++ pinterest-clone/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,17 @@
+import NextAuth from "next-auth"
+import GoogleProvider from "next-auth/providers/google";
+
+if (!process.env.GOOGLE_CLIENT_ID || !process.env.GOOGLE_CLIENT_SECRET) {
+    throw new Error("Missing GOOGLE_CLIENT_ID or GOOGLE_CLIENT_SECRET");
+  }
+
+const handler = NextAuth({
+    providers: [
+        GoogleProvider({
+          clientId: process.env.GOOGLE_CLIENT_ID,
+          clientSecret: process.env.GOOGLE_CLIENT_SECRET
+        })
+      ]
+})
+
+export { handler as GET, handler as POST }
